0ec48a7dd0490fade11633449c81ca0d198c43a7
MaryBark <97518012+MaryBark@users.noreply.github.com>
2023-01-10 13:19:50 +0300

    Add files via upload

---
+++ auxiliary.h
@@ -0,0 +1,20 @@
+#ifndef AUXILIARY_H
+#define AUXILIARY_H
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <functional>
+
+
+
+using func_str = std::function<void(std::string)>;
+
+
+std::vector<std::string> split(const std::string& str, char d);
+
+void read_lines(std::istream& stream, func_str fn_line_handler);
+
+
+#endif // AUXILIARY_H
